a3d00bf80507945a2fdf8df03170cac9ea250241
Dinakarasu <selvadina17@gmail.com>
2026-04-25 19:05:28 +0530

    add Main.java embedded tomcat

---
+++ src/main/java/com/website/Main.java
@@ -0,0 +1,21 @@
+package com.website;
+
+import java.io.File;
+import org.apache.catalina.startup.Tomcat;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        String port = System.getenv("PORT");
+        if (port == null) port = "8080";
+        
+        Tomcat tomcat = new Tomcat();
+        tomcat.setPort(Integer.parseInt(port));
+        tomcat.getConnector();
+        
+        String webappDir = new File("src/main/webapp").getAbsolutePath();
+        tomcat.addWebapp("", webappDir);
+        
+        tomcat.start();
+        tomcat.getServer().await();
+    }
+}
