bd2d0a2a5b4d1601704e5b74658145c6dc8a0124
Dinakarasu <selvadina17@gmail.com>
2026-04-25 22:10:34 +0530

    extract webapp from jar correctly

--- src/main/java/com/website/Main.java
+++ src/main/java/com/website/Main.java
@@ -2,6 +2,8 @@ package com.website;
 
 import java.io.*;
 import java.net.URL;
+import java.util.Enumeration;
+import java.util.jar.*;
 import org.apache.catalina.Context;
 import org.apache.catalina.startup.Tomcat;
 
@@ -14,12 +16,43 @@ public class Main {
         tomcat.setPort(Integer.parseInt(port));
         tomcat.getConnector();
 
-        // Extract webapp from inside JAR to temp directory
+        // Extract webapp from JAR to temp directory
         File tempDir = new File(System.getProperty("java.io.tmpdir"), "webapp");
+        if (tempDir.exists()) deleteDir(tempDir);
         tempDir.mkdirs();
-        extractWebapp(tempDir);
 
-        System.out.println("Webapp extracted to: " + tempDir.getAbsolutePath());
+        // Get the JAR file path
+        String jarPath = Main.class.getProtectionDomain()
+            .getCodeSource().getLocation().toURI().getPath();
+        System.out.println("JAR path: " + jarPath);
+
+        // Extract all webapp/* files from JAR
+        JarFile jar = new JarFile(jarPath);
+        Enumeration<JarEntry> entries = jar.entries();
+        while (entries.hasMoreElements()) {
+            JarEntry entry = entries.nextElement();
+            String name = entry.getName();
+            if (name.startsWith("webapp/")) {
+                File outFile = new File(tempDir,
+                    name.substring("webapp/".length()));
+                if (entry.isDirectory()) {
+                    outFile.mkdirs();
+                } else {
+                    outFile.getParentFile().mkdirs();
+                    try (InputStream in = jar.getInputStream(entry);
+                         FileOutputStream out = new FileOutputStream(outFile)) {
+                        byte[] buf = new byte[4096];
+                        int len;
+                        while ((len = in.read(buf)) > 0) {
+                            out.write(buf, 0, len);
+                        }
+                    }
+                }
+            }
+        }
+        jar.close();
+        System.out.println("Extracted to: " + tempDir.getAbsolutePath());
+        System.out.println("Files: " + tempDir.listFiles().length);
 
         Context ctx = tomcat.addWebapp("", tempDir.getAbsolutePath());
         ctx.setReloadable(false);
@@ -28,21 +61,10 @@ public class Main {
         tomcat.getServer().await();
     }
 
-    private static void extractWebapp(File targetDir) throws Exception {
-        URL resourceUrl = Main.class.getClassLoader().getResource("webapp");
-        if (resourceUrl == null) {
-            System.out.println("No webapp resource found in classpath!");
-            return;
+    private static void deleteDir(File dir) {
+        if (dir.isDirectory()) {
+            for (File f : dir.listFiles()) deleteDir(f);
         }
-
-        String jarPath = resourceUrl.getPath();
-        System.out.println("Resource URL: " + jarPath);
-
-        // Copy all webapp resources
-        java.util.Enumeration<URL> resources = Main.class.getClassLoader()
-            .getResources("webapp");
-        while (resources.hasMoreElements()) {
-            System.out.println("Found: " + resources.nextElement());
-        }
+        dir.delete();
     }
 }
